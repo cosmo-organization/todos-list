dad6117dbe562ba44b952f23be7e6a42fc3f7842
cosmo-organization <sonuaryan76448002@gmail.com>
2022-01-31 10:36:09 +0530

    Changes done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,23 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import {JoinRouteBase} from "../utils/route_joiner";
+import {Link} from 'react-router-dom';
 
 export default function Header(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href={props.baseLink}>{props.title}</a>
+				<Link to="" className="navbar-brand">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href={props.homeLink}>Home</a>
+							<Link to="" className="nav-link active" aria-current="page">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href={JoinRouteBase(props.baseLink,props.aboutLink)}>About</a>
+							<Link to="about" className="nav-link" aria-current="page">About</Link>
                         </li>
                     </ul>
                     {props.searchBar?<form className="d-flex">
